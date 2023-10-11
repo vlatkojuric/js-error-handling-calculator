a03384d18aa1a26d8254004ff0dea679cf3c8790
Vlatko Juric <juricv78gmail.com>
2023-10-11 12:15:44 +0200

    challenge done

--- js/index.js
+++ js/index.js
@@ -8,10 +8,14 @@ const operations = {
   subtract: (a, b) => a - b,
   multiply: (a, b) => a * b,
   divide: (a, b) => {
-    if (b === 0) {
-      throw new Error("Cannot divide by zero!");
+    try {
+      if (b === 0) {
+        throw new Error("Cannot divide by zero!");
+      }
+      return a / b;
+    } catch (error) {
+      console.log(error.message);
     }
-    return a / b;
   },
 };
 
